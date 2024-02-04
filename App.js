4cf419721912055e84cc8d2913e73f5eb4cc92a6
Darell Hoei <darellhoei@gmail.com>
2024-02-05 00:20:46 +0700

    list of pokemons

--- App.js
+++ App.js
@@ -1,36 +1,64 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Header, ListItem } from "@rneui/themed";
+import { useEffect, useState } from "react";
+import { Text, View, FlatList, StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { fetchPokemonList } from "./helpers/fetchPokemonList";
-import { useEffect, useState } from "react";
 
 export default function App() {
   const [pokemonData, setPokemonData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setPokemonData(fetchPokemonList());
+    fetchPokemonList().then((data) => {
+      console.log(data);
+      setPokemonData(data);
+      setIsLoading(false);
+    });
   }, []); // Only fetch once when the component mounts
 
-  console.log("pokemon data:", pokemonData);
+  // console.log("pokemon data:", pokemonData.results[0]);
+
+  const renderItem = ({ item }) => (
+    <ListItem bottomDivider>
+      <ListItem.Content>
+        <ListItem.Title>{item.name}</ListItem.Title>
+      </ListItem.Content>
+      <ListItem.Chevron />
+    </ListItem>
+  );
+
+  let content;
+
+  if (isLoading) {
+    content = <Text>Loading...</Text>;
+  } else {
+    content = (
+      <FlatList
+        data={pokemonData.results}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.name}
+      />
+    );
+  }
 
   return (
-    <View>
-      {/* Display a loading indicator while data is being fetched */}
-      {pokemonData === null && <Text>Loading...</Text>}
-
-      {/* Render the Pokémon data when available */}
-      {pokemonData && (
-        <Text>Fetched Pokémon data: {JSON.stringify(pokemonData)}</Text>
-      )}
-    </View>
+    <SafeAreaProvider>
+      <Header
+        centerComponent={{
+          text: "Pokedex Mobile",
+          style: styles.heading,
+        }}
+      />
+      <View style={styles.container}>{content}</View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  heading: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
